0a094de30593f79a7fe816cb462e57c3c4cc43da
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-07-03 01:02:32 -0700

    fort

---
+++ server/router/auth-router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controller/auth-controller");
+
+router.route("/product").get(authController.product);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,23 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.status(200).send("WELLCOME IS PARCTICE SECTION !");
-// });
-// app.get("/RES", (req, res) => {
-//     res.status(200).send("WELLCOME IS PARCTICE SECTION !");
-// });
-// const PORT = 7000;
-// app.listen(PORT, () => {
-//     console.log(`running on ${PORT}`)
-// });
-
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const authRoutes = require("./router/auth-router"); 
+
+
+ app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRoutes); 
 
 app.get("/", (req, res) => {
-    res.status(200).send("WELCOME TO PRACTICE SECTION!");
-});
-
-app.get("/RES", (req, res) => {
-    res.status(200).send("WELCOME TO PRACTICE SECTION /RES!");
+  res.status(200).send("WELCOME TO PRACTICE SECTION!");
 });
 
 const PORT = 7000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
