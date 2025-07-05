4d08fbc2860cdf512821cd97953eda44d04411c5
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-07-05 03:44:34 -0700

    Add Product folder

---
+++ server/controller/Products/first.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: "1",
+    name: "Babar",
+    Address: "New York",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRdvA71PY0tb6pUjhOZHOAfikh56Nc7q4-PQ&s",
+    phone: "31",
+  },
+  {
+    id: "2",
+    name: "Ali",
+    Address: "London",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlBKeP72xZlrWk2E-YtJ4hhoZ0zfFJmr9LCg&s",
+    phone: "20",
+  },
+  {
+    id: "3",
+    name: "Ahmed",
+    Address: "Paris",
+    img: "https://aerospaceamerica.aiaa.org/wp-content/uploads/2020/12/psych-opener.jpg",
+    phone: "13",
+  },
+   {
+    id: "4",
+    name: "Shaheen",
+    Address: "Pakistan",
+    img: "https://nstxl.org/wp-content/uploads/2023/03/Untitled-design-27.png",
+    phone: "42",
+  }
+];
+
+module.exports = products;
--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,22 +1,13 @@
-  const product = async (req, res) => {
+const products = require("./Products/first");
+
+const product = async (req, res) => {
   try {
-    res.status(200).json({
-      id: "1",
-      name: "babar",
-      Address: "New York",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRdvA71PY0tb6pUjhOZHOAfikh56Nc7q4-PQ&s",
-      phone: "1",
-    },
-  );
-    console.log("success");
+    res.status(200).json(products);
+    console.log("Products sent successfully");
   } catch (error) {
     console.error("Product Error:", error);
     res.status(500).send("Something went wrong in product route.");
   }
-
-
 };
 
 module.exports = { product };
-
-
